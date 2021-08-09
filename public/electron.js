6b5558465c926ace36df354199b4d22d41bf89a5
JinOhKwon <kjo1333@gmail.com>
2021-08-10 00:06:41 +0900

    일렉트론 기본 완료

--- public/electron.js
+++ public/electron.js
@@ -3,8 +3,31 @@ exports.__esModule = true;
 var electron_1 = require("electron");
 var isDev = require("electron-is-dev");
 var path = require("path");
+/**
+ * 메인 윈도우
+ */
 var mainWindow;
-var createWindow = function () {
+/**
+ * ipc메인을 초기화 한다.
+ */
+var ipcMainInit = function () {
+    electron_1.ipcMain.handle('dark-mode:toggle', function () {
+        if (electron_1.nativeTheme.shouldUseDarkColors) {
+            electron_1.nativeTheme.themeSource = 'light';
+        }
+        else {
+            electron_1.nativeTheme.themeSource = 'dark';
+        }
+        return electron_1.nativeTheme.shouldUseDarkColors;
+    });
+    electron_1.ipcMain.handle('dark-mode:system', function () {
+        electron_1.nativeTheme.themeSource = 'system';
+    });
+};
+/**
+ * 일렉트론을 초기화 한다.
+ */
+var createElectronInit = function () {
     mainWindow = new electron_1.BrowserWindow({
         width: 900,
         height: 680,
@@ -13,6 +36,7 @@ var createWindow = function () {
         resizable: true,
         fullscreen: false,
         fullscreenable: true,
+        title: 'xxxx',
         webPreferences: {
             // node환경처럼 사용하기
             nodeIntegration: true,
@@ -21,6 +45,7 @@ var createWindow = function () {
             devTools: isDev
         }
     });
+    ipcMainInit();
     // production에서는 패키지 내부 리소스에 접근.
     // 개발 중에는 개발 도구에서 호스팅하는 주소에서 로드.
     mainWindow.loadURL(isDev ? 'http://localhost:3000' : "file://" + path.join(__dirname, '../build/index.html'));
@@ -28,15 +53,16 @@ var createWindow = function () {
         mainWindow.webContents.openDevTools({ mode: 'detach' });
     }
     mainWindow.setResizable(true);
-    // Emitted when the window is closed.
     mainWindow.on('closed', function () { return (mainWindow = undefined); });
     mainWindow.focus();
 };
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-electron_1.app.on('ready', createWindow);
-// Quit when all windows are closed.
+/**
+ * 일렉트론을 실행한다.
+ */
+electron_1.app.on('ready', createElectronInit);
+/**
+ * 종료한다.
+ */
 electron_1.app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         electron_1.app.quit();
@@ -44,6 +70,6 @@ electron_1.app.on('window-all-closed', function () {
 });
 electron_1.app.on('activate', function () {
     if (mainWindow === null) {
-        createWindow();
+        createElectronInit();
     }
 });
--- public/electron.ts
+++ public/electron.ts
@@ -1,12 +1,36 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain, nativeTheme } from 'electron';
 import * as isDev from 'electron-is-dev';
 import * as path from 'path';
 
+/**
+ * 메인 윈도우
+ */
 let mainWindow: BrowserWindow;
 
-const createWindow = () => {
+/**
+ * ipc메인을 초기화 한다.
+ */
+const ipcMainInit = () => {
+	ipcMain.handle('dark-mode:toggle', () => {
+		if (nativeTheme.shouldUseDarkColors) {
+			nativeTheme.themeSource = 'light'
+		} else {
+			nativeTheme.themeSource = 'dark'
+		}
+		return nativeTheme.shouldUseDarkColors
+	})
+
+	ipcMain.handle('dark-mode:system', () => {
+		nativeTheme.themeSource = 'system'
+	})
+}
+
+/**
+ * 일렉트론을 초기화 한다.
+ */
+const createElectronInit = () => {
 	mainWindow = new BrowserWindow({
 		width: 900,
 		height: 680,
@@ -23,7 +47,8 @@ const createWindow = () => {
 			devTools: isDev,
 		},
 	});
-
+	
+	ipcMainInit();
 	// production에서는 패키지 내부 리소스에 접근.
 	// 개발 중에는 개발 도구에서 호스팅하는 주소에서 로드.
 	mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
@@ -33,18 +58,18 @@ const createWindow = () => {
 	}
 
 	mainWindow.setResizable(true);
-
-	// Emitted when the window is closed.
 	mainWindow.on('closed', () => (mainWindow = undefined!));
 	mainWindow.focus();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+/**
+ * 일렉트론을 실행한다.
+ */
+app.on('ready', createElectronInit);
 
-// Quit when all windows are closed.
+/**
+ * 종료한다.
+ */
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
 		app.quit();
@@ -53,6 +78,6 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
 	if (mainWindow === null) {
-		createWindow();
+		createElectronInit();
 	}
 });
